f021f6882fd3f246ceb61d24060e3aaacb34c314
Hardik-Ubhrani <businessbrat99@gmail.com>
2025-11-29 00:48:47 +0530

    Tower of Hanoi.c

--- Tower of Hanoi.c
+++ Tower of Hanoi.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-void hardik_hanoi_10566(int,int,int);
+void hardik_hanoi(int,int,int);
 int disp(int,int);
 void main()
 {
@@ -10,20 +10,20 @@ void main()
     scanf("%d",&start);
     printf("Enter destination rod");
     scanf("%d",&end);
-    hardik_hanoi_10566(ring,start,end);
+    hardik_hanoi(ring,start,end);
 }
 
 
-void hardik_hanoi_10566(int ring,int start,int end)
+void hardik_hanoi(int ring,int start,int end)
 //ring: defines number of rings, start:defines the starting rod,end:defines the end rod
 {
     if (ring == 1)
      disp(start,end);
     else {
         int other=6-start-end;
-        hardik_hanoi_10566(ring-1,start,other);
+        hardik_hanoi(ring-1,start,other);
         disp(start,end);
-        hardik_hanoi_10566(ring-1,other,end);
+        hardik_hanoi(ring-1,other,end);
     }
 
 
